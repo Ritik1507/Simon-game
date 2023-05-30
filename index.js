65b70f2e5b4386c93c24df5de1589a4fedce22cb
ritik padhi <ritikpadhi1507@@example.com>
2023-05-30 17:09:32 +0530

    done

--- index.js
+++ index.js
@@ -1,44 +1,90 @@
 
-var buttonColors = ["red","blue","green","yellow"] ;
+
+var buttonColor = ["red", "blue", "green", "yellow"]
 var gamePattern = [];
 var userClickedPattern = [];
 
-$(".btn").click(function(){
+var started = false;
 
+var level = 0;
+
+
+$(document).keypress(function () {
+
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+
+$(".btn").click(function () {
     var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
 
     playSound(userChosenColor);
 
-    animatePress(userChosenColor);
+    animatePressed(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
 });
 
-function nextSequence(){
-   
-    var nextNumber = Math.floor(Math.random()*4);
-    var randomChosenColor = buttonColors[nextNumber];
+
+function checkAnswer(currentlevel) {
+
+    if (gamePattern[currentlevel] === userClickedPattern[currentlevel]) {
+        console.log("success");
+        if (userClickedPattern.length === gamePattern.length) {
+
+          setTimeout(function () {
+          nextSequence();
+          }, 1000);
+        }
+
+
+    }
+    else {
+        console.log("wrong");
+    }
+
+
+}
+
+function nextSequence() {
+    userClickedPattern = [];
+
+    level++;
+
+    $("#level-title").text("Level " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColor[randomNumber];
     gamePattern.push(randomChosenColor);
 
-    $("#"+randomChosenColor).fadein(100).fadeout(100).fadein(100);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
     playSound(randomChosenColor);
+
 }
 
-function playSound(name){
-    var audio = new Audio("sounds/"+ name +".mp3");
+
+
+function playSound(currentButton) {
+
+    var audio = new Audio("sounds/" + currentButton + ".mp3");
+
     audio.play();
+
 }
 
-
-
-function animatePress(currentColor) {
-
+function animatePressed(currentColor) {
 
     $("#" + currentColor).addClass("pressed");
-  
 
     setTimeout(function () {
-      $("#" + currentColor).removeClass("pressed");
+
+        $("#" + currentColor).removeClass("pressed");
+
     }, 100);
 }
 
-    
